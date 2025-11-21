9110e6a79125359fdab78a1e1008efd605f34f44
Nicolas Barbey <nicolas.a.barbey@gmail.com>
2025-11-21 16:49:24 +0100

    set-up testify tests

---
+++ internal/internal_test.go
@@ -0,0 +1,11 @@
+package internal
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestInternal(t *testing.T) {
+	t.Skip()
+	assert.True(t, false)
+}
---
+++ pkg/pkg_test.go
@@ -0,0 +1,11 @@
+package pkg
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestPkg(t *testing.T) {
+	t.Skip()
+	assert.True(t, false)
+}
