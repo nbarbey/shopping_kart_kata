29304d81f83f5ebb7f710360a6b300fcc598b4cb
Axel Maulion <axel.maulion@adevinta.com>
2025-12-01 12:02:43 +0100

    add products to shopping cart WIP

--- pkg/pkg.go
+++ pkg/pkg.go
@@ -1,21 +1,56 @@
 package pkg
 
+import (
+	"fmt"
+
+	money "github.com/Rhymond/go-money"
+)
+
+type Item string
+
+type Quantity int
+
+type Items map[Item]Quantity
+
 type ShoppingCart struct {
+	products Products
+	items    Items
 }
 
 func (c ShoppingCart) String() string {
-	return `
---------------------------------------------
+	const (
+		header = `--------------------------------------------
 | Product Name | Price with VAT | Quantity |
-|--------------|----------------|----------|
-|------------------------------------------|
+|--------------|----------------|----------|`
+
+		promotion = `|------------------------------------------|
 | Promotion:                               |
-|------------------------------------------|
+|------------------------------------------|`
+	)
+
+	return fmt.Sprintf(`
+%s
+%s
 | Total products: 0                        |
 | Total price: 0.00 €                      |
---------------------------------------------`
+--------------------------------------------`, header, promotion)
 }
 
 func NewShoppingCart() *ShoppingCart {
-	return &ShoppingCart{}
+	cart := &ShoppingCart{
+		products: NewProducts(),
+		items:    make(Items),
+	}
+	cart.products.Add(Product{
+		Name:              "Iceberg",
+		Cost:              money.New(155, money.EUR),
+		PercentageRevenue: 15,
+		TaxPecentage:      21,
+	})
+
+	return cart
+}
+
+func (c *ShoppingCart) Add(s string) {
+	c.items[Item(s)]++
 }
--- pkg/pkg_test.go
+++ pkg/pkg_test.go
@@ -24,20 +24,22 @@ func TestEmptyCart(t *testing.T) {
 	assert.Equal(t, expected, result)
 }
 
-// func TestCart(t *testing.T) {
-// 	cart := NewShoppingCart()
+func TestCart_WithOneProduct(t *testing.T) {
+	cart := NewShoppingCart()
+	cart.Add("Iceberg")
 
-// 	result := cart.String()
+	result := cart.String()
 
-// 	expected := `
-// --------------------------------------------
-// | Product Name | Price with VAT | Quantity |
-// |--------------|----------------|----------|
-// |------------------------------------------|
-// | Promotion:                               |
-// |------------------------------------------|
-// | Total products: 0                        |
-// | Total price: 0.00 €                      |
-// --------------------------------------------`
-// 	assert.Equal(t, expected, result)
-// }
+	expected := `
+--------------------------------------------
+| Product Name | Price with VAT | Quantity |
+|--------------|----------------|----------|
+| Iceberg	   | 2.17 € 		| 1	   	   |
+|------------------------------------------|
+| Promotion:                               |
+|------------------------------------------|
+| Total products: 1                        |
+| Total price: 2.17 €                      |
+--------------------------------------------`
+	assert.Equal(t, expected, result)
+}
